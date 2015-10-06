8667d2cf17f678f427e7083032b167d5dd678294
kapetance <kapetance@gmail.com>
2015-10-06 09:53:16 +0200

    Exercise 05-17

--- 05 - Arrays/05-17.js
+++ 05 - Arrays/05-17.js
@@ -1,3 +1,37 @@
 /*
 Write a JavaScript program to shuffle an array.
 */
+"use strict";
+(function () {
+    Array.prototype.shuffle = function () {
+        function tempRandom() {
+            var x = [];
+            var y = [];
+            for (var z = 0; z < this.length; z++) {
+                x.push(z);
+            }
+            for (var n = 0; n < this.length; n++) {
+                y.push(x.splice(Math.floor(Math.random() * x.length), 1));
+            }
+            return y;
+        }
+
+        var finalArray = [];
+        var randomArray = tempRandom.call(this);
+        for (var x = 0; x < this.length; x++) {
+            finalArray.push(this[randomArray[x]]);
+        }
+        return finalArray;
+    }
+
+    function gener(length) {
+        var arka = [];
+        for (var x = 0; x < length; x++) {
+            arka.push(x);
+        }
+        return arka;
+    }
+
+    var pickedArray = gener(1000)
+    console.log(pickedArray.shuffle());
+})();
