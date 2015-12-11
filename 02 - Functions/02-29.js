347fabfd3a978d3387d2c6d536b19f2eab963a1b
kapetance <kapetance@gmail.com>
2015-12-11 15:18:55 +0100

    11-12-2015

--- 02 - Functions/02-29.js
+++ 02 - Functions/02-29.js
@@ -1,3 +1,11 @@
 /*
 Write a JavaScript function to get the function name.
 */
+"use strict";
+(function () {
+    function nameFunction(funct) {
+        console.log(funct.name);
+    }
+    
+    nameFunction(function test() {});
+})();
