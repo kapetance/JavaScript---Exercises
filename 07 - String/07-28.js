db93cc614a5e81456c54637dc770ea23f08d707d
kapetance <kapetance@gmail.com>
2015-12-14 20:43:39 +0100

    14-12-2015

--- 07 - String/07-28.js
+++ 07 - String/07-28.js
@@ -7,3 +7,15 @@ Output :
 "12"
 "100"
 */
+"use strict";
+(function () {
+    function hexToAscii(pickedAscii) {
+        var asciiArray = [];
+        for (var x = 0; x < pickedAscii.length / 2; x++) {
+            asciiArray.push(parseInt(pickedAscii.slice(2 * x, 2 * x + 2), 16));
+        }
+        return String.fromCharCode.apply(null, asciiArray);
+    }
+    console.log(hexToAscii('3132'));
+    console.log(hexToAscii('313030'));
+})();
