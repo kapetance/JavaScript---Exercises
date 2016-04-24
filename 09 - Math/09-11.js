ff6ca8ba18258db8cddf53ef72a66539403b0366
kapetance <kapetance@gmail.com>
2016-04-24 18:24:44 +0200

    24042016

--- 09 - Math/09-11.js
+++ 09 - Math/09-11.js
@@ -5,3 +5,21 @@ console.log(lcm_more_than_two_numbers([5,10,15,25]));
 Output :
 25200
 150  */
+"use strict";
+(function () {
+    function lcm(numbers) {
+        var lowest = Math.min.apply(null, numbers);
+        var highest = numbers.reduce((x, y) => {
+            return x * y;
+        });
+        for (var x = 1; x <= highest; x++) {
+            if (numbers.every((s) => {
+                return x * lowest % s === 0;
+            })) {
+                return x * lowest;
+            }
+        }
+    }
+    console.log(lcm([100, 90, 80, 7]));
+    console.log(lcm([5, 10, 15, 25]));
+})();
