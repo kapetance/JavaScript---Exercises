3926664044879e221c1b6230da87e24b8d61ffd0
kapetance <kapetance@gmail.com>
2016-05-11 08:27:01 +0200

    11052016

--- 08 - Validation with Regular Expression/08-01.js
+++ 08 - Validation with Regular Expression/08-01.js
@@ -1,1 +1,6 @@
 /*Write a JavaScript program to test the first character of a string is uppercase or not.*/
+"use strict";
+(function () {
+    var patern = /^[A-Z]/;
+    console.log(patern.test("Test case"));
+})();
--- 08 - Validation with Regular Expression/08-02.js
+++ 08 - Validation with Regular Expression/08-02.js
@@ -1,1 +1,7 @@
 /*Write a JavaScript program to check a credit card number. */
+(function () {
+    var patern = /(\d{4}-){3}\d{4}/;
+    console.log(patern.test("5673-2342-4324-4324"));
+    console.log(patern.test("5673-2as2-43-4324"));
+    console.log(patern.test("5673-2342-43782-432"));
+})();
