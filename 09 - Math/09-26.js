f3ae238e9bcd106c2ebb729ff778930b00a852ca
kapetance <kapetance@gmail.com>
2016-08-04 14:01:40 +0200

    04082016

--- 09 - Math/09-26.js
+++ 09 - Math/09-26.js
@@ -4,3 +4,12 @@ console.log(nthroot(64, 2));
 8
 console.log(nthroot(64, -2));
 0.125 */
+
+(function () {
+    function nthRoot(pickedNumber, root) {
+        return Math.pow(pickedNumber, 1/root);
+    }
+    
+    console.log(nthRoot(64, 2));
+    console.log(nthRoot(64, -2));
+})();
