0eeb5fa4d00b438ad1ea52792ade3b4e72bbe9aa
kapetance <kapetance@gmail.com>
2016-08-11 15:31:35 +0200

    11082016

--- 03 - Recursions/03-07.js
+++ 03 - Recursions/03-07.js
@@ -1,3 +1,16 @@
 /*
 Write a JavaScript program to check whether a number is even or not.
 */
+(function () {
+    function evenOdd(pickedNumber) {
+        if (pickedNumber===0) {
+            return "Paran";
+        } else if (pickedNumber === 1){
+            return "Neparan";
+        } else {
+            return evenOdd(pickedNumber-2);
+        }
+    }
+    
+    console.log(evenOdd(26));
+})();
